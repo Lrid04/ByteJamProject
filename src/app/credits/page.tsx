24ad05b19ac90b6f4ace428c32c82fe56d553012
Jaxon-Ford <148885484+Jaxon-Ford@users.noreply.github.com>
2024-11-06 15:21:53 -0600

    added photos and put them on contacts page

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -7,34 +7,34 @@ export default function Credits() {
       <NavBar />
       <div className="flex justify-around grid grid-col-4 grid-rows-3 grid-flow-col gap-4 text-center">
         <h1 className="col-span-3 content-center text-5xl">Thank you for all of our developers!</h1>
-        <div className="w-60 border-2">
+        <div className="w-60 border-2 h-fit">
           <h1>Jaxon</h1>
-          <Image src={""} alt="Jaxon" className="h-52 "/>
+          <Image src={"/Jaxon.jpg"} alt="Jaxon" className="" width={236} height={100}/>
           <p className="">"I am greatful to be a part of this team"</p>
         </div>
-        <div className="w-60 border-2">
+        <div className="w-60 border-2 h-fit">
           <h1>Landon</h1>
-          <Image src={""} alt="Landon" className="h-52 "/>
+          <Image src={"/Landon.jpg"} alt="Nixon" className="" width={236} height={100}/>
           <p>...</p>
         </div>
-        <div className="w-60 border-2">
+        <div className="w-60 border-2 h-fit">
           <h1>Guyvani</h1>
-          <Image src={""} alt="Guyvani" className="h-52"/>
+          <Image src={""} alt="Guyvani" className="" width={236} height={100}/>
           <p>...</p>
         </div>
-        <div className="w-60 border-2">
+        <div className="w-60 border-2 h-fit">
           <h1>daniel</h1>
-          <Image src={""} alt="daniel" className="h-52"/>
+          <Image src={""} alt="daniel" className="" width={236} height={100}/>
           <p>...</p>
         </div>
-        <div className="w-60 border-2">
+        <div className="w-60 border-2 h-fit">
           <h1>Nixon</h1>
-          <Image src={""} alt="Nixon" className="h-52"/>
+          <Image src={"/Nixon.JPG"} alt="Nixon" className="" width={236} height={100}/>
           <p>...</p>
         </div>
-        <div className="w-60 border-2">
-          <h1>#</h1>
-          <Image src={""} alt="#" className="h-52"/>
+        <div className="w-60 border-2 h-fit">
+          <h1>Meghan</h1>
+          <Image src={"/Meghan.jpg"} alt="#" className="" width={236} height={100}/>
           <p>...</p>
         </div>
       </div>
