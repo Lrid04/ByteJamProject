37feebb5f6f650fac52124ed7d7cd64c97c6c179
DanielBlidchenko <dblidchenko2@gmail.com>
2024-11-06 11:43:49 -0800

    nav bar

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -5,9 +5,9 @@ export default function NavBar() {
       <nav className="bg-gray-800 p-1 text-slate-400 ">
         <div className="flex justify-end flex space-x-2">
         <h1 className="text-xl font-bold text-white mb-4 w-full">My Website</h1>
-        <Link href="/Home" className="underline">Home</Link>
-        <Link href="/Machine" className="underline">Machine</Link>
-        <Link href="/Credits"className="underline">Credits</Link>
+        <Link href="/" className="underline">Home</Link>
+        <Link href="/machine" className="underline">Machine</Link>
+        <Link href="/credits"className="underline">Credits</Link>
         </div>
       </nav>
     );
