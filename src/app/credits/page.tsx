0e7926f517419d05982c221ff9795b41559fbb42
Jaxon-Ford <148885484+Jaxon-Ford@users.noreply.github.com>
2024-11-07 10:40:32 -0600

    Finished photos and quotes

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -15,27 +15,27 @@ export default function Credits() {
         <div className="w-60 border-2 h-fit">
           <h1>Landon</h1>
           <Image src={"/Landon.jpg"} alt="Nixon" className="" width={236} height={100}/>
-          <p>...</p>
+          <p>"Leading may be hard, but also very rewarding"</p>
         </div>
         <div className="w-60 border-2 h-fit">
           <h1>Guyvani</h1>
-          <Image src={""} alt="Guyvani" className="" width={236} height={100}/>
-          <p>...</p>
+          <Image src={"/Guyvanii.png"} alt="Guyvani" className="" width={236} height={100}/>
+          <p>"Working with this team is really a moment to evolve and learn and become a Familly"</p>
         </div>
         <div className="w-60 border-2 h-fit">
-          <h1>daniel</h1>
-          <Image src={""} alt="daniel" className="" width={236} height={100}/>
-          <p>...</p>
+          <h1>Daniel</h1>
+          <Image src={"/Daniel.jpeg"} alt="Daniel" className="" width={236} height={100}/>
+          <p>I am greatful for this experiece, and this team</p>
         </div>
         <div className="w-60 border-2 h-fit">
           <h1>Nixon</h1>
           <Image src={"/Nixon.JPG"} alt="Nixon" className="" width={236} height={100}/>
-          <p>...</p>
+          <p>"It's fun working with everyone, very enjoyable"</p>
         </div>
         <div className="w-60 border-2 h-fit">
           <h1>Meghan</h1>
           <Image src={"/Meghan.jpg"} alt="#" className="" width={236} height={100}/>
-          <p>...</p>
+          <p>"I’m thrilled to dive into the React framework for my JavaScript project in Visual Studio! Collaborating with my team using Node and Git to bring our webpage to life!"</p>
         </div>
       </div>
     </main>
