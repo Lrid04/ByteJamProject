72bf6a4a87a18ead297f45256622115d813b18b6
Nixon Thompson <nixon@Nixons-MacBook-Air.local>
2024-11-07 09:48:01 -0600

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,50 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p></p>
+    <div>
+      <p>
+      It was a dreary cold November night when you got home 
+      from work. Tired and freezing as you walked into your house, 
+      which was not much warmer than the outside. It was dark 
+      inside, you shuffled off the living room to spend your time 
+      before you inevitably had to return to work in the morning. 
+      Had to find something fun to do, maybe read a book before 
+      you have your dinner. 
+      <br />
+      You plopped down in your armchair with a sigh, reaching over 
+      to fumble with a lamp on the table next to you and twisting 
+      the knob, and filling the room with light. When you looked back 
+      from the lamp, you jumped not expecting what was sitting before 
+      you, something that was not usually in your living room. Where 
+      could this have come from? You stood up walking closer to 
+      examine the object.
+      <br />
+      It was one of those ‘TVs’ that everybody had started to buy, 
+      boxes with screens that could display an image. You thought it 
+      was a very impressive concept, but you could never buy one for 
+      yourself. It was too expensive—hell, they were like $100! So how 
+      did this possibly end up in your living room? You certainly didn’t 
+      buy it.
+      <br />
+      On top of the set was a remote, certainly not like the ones 
+      you’ve seen before from other’s TV’s.
+      <br />
+      Why was this here? You lived alone, just you in this house, you 
+      certainly didn’t sleep buy a TV! Where did it come from? It’s 
+      not like you could complain much, the strange TV was free. At 
+      least it was something to do.
+      <br />
+      Why not try it?
+      <br />
+      You picked up the remote, a bulky metal clicker with numbered 
+      buttons. Cold against your hand, you stare at the TV a weird 
+      feeling building in your chest. This all was very strange, but 
+      your curiosity was getting the best of you. You took a seat in 
+      your armchair, you stared at the remote for a bit, it weighed 
+      heavily on your mind. You had to find out what this was.
+      <br />
+      So holding your breath you pressed the button.
+      </p>
+    </div>
   );
 }
