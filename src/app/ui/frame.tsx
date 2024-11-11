92e54499c9fd096f0b8b2f9595dc2cd5b4a3bf3e
Jaxon-Ford <148885484+Jaxon-Ford@users.noreply.github.com>
2024-11-11 12:25:29 -0600

    delete unnecessary code

--- src/app/ui/frame.tsx
+++ src/app/ui/frame.tsx
@@ -1,7 +1,7 @@
 export default function Frame() {
     return (
         <main className='bg-[url("/frame.png")] bg-contain bg-no-repeat h-[50%] grow pt-[19%]'>
-            <pre className="text-wrap text-right pr-[21%] pl-[19%] mb-[19%] overflow-y-scroll overflow-hidden h-[47%] object-fit: scale-down;">
+            <pre className="text-wrap text-right pr-[21%] pl-[19%] mb-[19%] overflow-y-scroll h-[47%]">
                 This is a sample text
                 <br />
                 This is a sample text
