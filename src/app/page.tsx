487f8880145ec6f8a7fa47c80f97ef26014f1f9b
nixon-04 <n.thompson021804@gmail.com>
2024-11-11 08:59:34 -0600

    Page shorten

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,56 +4,20 @@ export default function Home() {
   return (
     <main>
       <NavBar />
-      <div className="text-lg text-center pr-20 pl-20 pt-10 italic"><p>
-      It was a dreary cold November night when you got home 
-      from work. Tired and freezing as you walked into your house, 
-      which was not much warmer than the outside. It was dark 
-      inside, you shuffled off the living room to spend your time 
-      before you inevitably had to return to work in the morning. 
-      Had to find something fun to do, maybe read a book before 
-      you have your dinner. 
-      <br />
-      <br />
-      You plopped down in your armchair with a sigh, reaching over 
-      to fumble with a lamp on the table next to you and twisting 
-      the knob, and filling the room with light. When you looked back 
-      from the lamp, you jumped not expecting what was sitting before 
-      you, something that was not usually in your living room. Where 
-      could this have come from? You stood up walking closer to 
-      examine the object.
-      <br />
-      <br />
-      It was one of those ‘TVs’ that everybody had started to buy, 
-      boxes with screens that could display an image. You thought it 
-      was a very impressive concept, but you could never buy one for 
-      yourself. It was too expensive—hell, they were like $100! So how 
-      did this possibly end up in your living room? You certainly didn’t 
-      buy it.
-      <br />
-      <br />
-      On top of the set was a remote, certainly not like the ones 
-      you’ve seen before from other’s TV’s.
-      <br />
-      <br />
-      Why was this here? You lived alone, just you in this house, you 
-      certainly didn’t sleep buy a TV! Where did it come from? It’s 
-      not like you could complain much, the strange TV was free. At 
-      least it was something to do.
-      <br />
-      <br />
-      Why not try it?
-      <br />
-      <br />
-      You picked up the remote, a bulky metal clicker with numbered 
-      buttons. Cold against your hand, you stare at the TV a weird 
-      feeling building in your chest. This all was very strange, but 
-      your curiosity was getting the best of you. You took a seat in 
-      your armchair, you stared at the remote for a bit, it weighed 
-      heavily on your mind. You had to find out what this was.
-      <br />
-      <br />
-      So holding your breath you pressed the button.
-      </p></div>
+      <div className="absolute text-4xl text-center m-20 italic">
+        <p className="m-20">
+        Exhausted from work, you collapsed into your armchair and 
+        fumbled with a lamp. When you turned back, you jumped 
+        in surprise at the sight of a boxy TV in your living 
+        room—that thing everybody had started to buy, boxes with 
+        screens that could display an image, something you thought
+         was too expensive at $100. Curious, you picked up the 
+         remote and sat down. 
+          <br />
+          <br />
+          You pressed the button.
+        </p>
+      </div>
     </main>
   );
 }
