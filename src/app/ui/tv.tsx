d0aeda95dfa119ce7a6577a13c7b1f401cb9eb51
Lrid04 <landon.riddik04@gmail.com>
2024-11-11 08:18:53 -0600

    Added Tv to machine screen, added frame picture

--- src/app/ui/tv.tsx
+++ src/app/ui/tv.tsx
@@ -2,13 +2,13 @@ import Image from "next/image"
 export default function Tv() {
     return (
       <main className='bg-[url("/tv.png")] bg-contain bg-no-repeat h-screen grow'>
-        <p className="pt-14 pl-64">Word</p>
+        <p className="" ></p>
         <Image 
         src={"/#"}
         alt={"Movie Poster"}
         width={100}
         height={100}
-        className="pt-24 pl-24"
+        className=""
         />
       </main>
     );
