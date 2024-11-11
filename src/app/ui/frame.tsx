8ff7d38c20d0b7a468a0110eb1961d4d7b7cb691
Jaxon-Ford <148885484+Jaxon-Ford@users.noreply.github.com>
2024-11-11 12:20:42 -0600

    fixed code unalignment

--- src/app/ui/frame.tsx
+++ src/app/ui/frame.tsx
@@ -9,22 +9,21 @@ theme: {
 export default function Frame() {
     return (
         <main className='bg-[url("/frame.png")] bg-contain bg-no-repeat h-[50%] grow pt-[19%]'>
-                <pre className="text-wrap text-right pr-[21%] pl-[19%] mb-[19%] overflow-y-scroll overflow-hidden h-[47%] object-fit: scale-down;">
-                    This is a sample text
-                    <br />
-                    This is a sample text
-                    <br />
-                    This is a sample text
-                    <br />
-                    This is a sample text
-                    <br />
-                    This is a sample text
-                    <br />
-                    This is a sample text
-                    <br />
-                    This is a sample text
-
-                </pre>
+            <pre className="text-wrap text-right pr-[21%] pl-[19%] mb-[19%] overflow-y-scroll overflow-hidden h-[47%] object-fit: scale-down;">
+                This is a sample text
+                <br />
+                This is a sample text
+                <br />
+                This is a sample text
+                <br />
+                This is a sample text
+                <br />
+                This is a sample text
+                <br />
+                This is a sample text
+                <br />
+                This is a sample text
+            </pre>
         </main>
 
     );
